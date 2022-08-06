8dc7136a493f1a5597dd52fbf2e2024dbf385c0b
Yaoyu Chen <yaoyuchen@eml.cc>
2022-08-06 10:19:08 +0100

    Create new page

---
+++ src/routes/app/schedule-preview.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
+	import { next } from '$lib/progress';
+
+	let shouldShow = false;
+
+	onMount(() => {
+		shouldShow = true;
+	});
+</script>
+
+{#if shouldShow}
+	<h2
+		class=" text-2xl font-semibold text-gray-800 text-center"
+		in:fly={{ y: 5, duration: 1000 }}
+		out:fly={{ y: -5, duration: 500 }}
+	>
+		Your 7-day bed time schedule.<br />
+	</h2>
+{/if}
