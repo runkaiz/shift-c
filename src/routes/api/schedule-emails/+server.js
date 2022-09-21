dee6846a580bb0b8d63f69873b02a4ab376d2ede
Runkai Zhang <me@runkaizhang.xyz>
2022-09-21 16:23:13 -0400

    Add some basic code to schedule emails with sendgrid

--- src/routes/api/schedule-emails/+server.js
+++ src/routes/api/schedule-emails/+server.js
@@ -1,4 +1,21 @@
+// TODO: Complete the email scheduling system.
+
+import sgMail from "@sendgrid/mail";
+// sgMail.setApiKey(import.meta.env.VITE_SENDGRID);
+
 /** @type {import('./$types').RequestHandler} */
-export function GET() {
-  return new Response("Mock: Scheduling emails...");
+
+export async function GET() {
+      const msg = {
+        to: "test@example.com",
+        from: "test@example.com",
+        subject: "Sending with SendGrid is Fun",
+        text: "and easy to do anywhere, even with Node.js",
+        html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+      };
+      console.log("Form submitted");
+      // const output = await sgMail.send(msg);
+  return {
+    body: output,
+  };
 }
