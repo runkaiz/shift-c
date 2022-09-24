f0ffcd2b220038674d4ef8b7856a1fb6a7c3e01a
Runkai Zhang <me@runkaizhang.xyz>
2022-09-24 17:13:14 -0400

    Add a mock result page to test out the UI design

---
+++ src/lib/components/Entries/Line.svelte
@@ -0,0 +1,1 @@
+<span class="absolute top-4 left-6 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
---
+++ src/lib/components/Entries/SleepTime.svelte
@@ -0,0 +1,48 @@
+<script>
+	import Line from "./Line.svelte";
+	
+	export let time = '';
+	export let date = '';
+	export let isLast = false;
+</script>
+
+<li>
+	<div class="relative pb-10">
+		{#if !isLast}
+			<Line />
+		{/if}
+		<div class="relative flex space-x-5">
+			<div>
+				<span
+					class="h-12 w-12 rounded-full bg-purple-400 flex items-center justify-center ring-8 ring-white"
+				>
+					<svg
+						class="h-8 w-8 text-white"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 24 24"
+						aria-hidden="true"
+						strokeWidth={1.5}
+						stroke="currentColor"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+						/>
+					</svg>
+				</span>
+			</div>
+			<div class="flex min-w-0 flex-1 justify-between space-x-4 pt-3">
+				<div>
+					<p class="text-sm text-gray-500">
+						Aim to sleep around <span class="font-medium text-gray-900">{time}</span>
+					</p>
+				</div>
+				<div class="whitespace-nowrap text-right text-sm text-gray-500">
+					<time datetime={date}>{date}</time>
+				</div>
+			</div>
+		</div>
+	</div>
+</li>
