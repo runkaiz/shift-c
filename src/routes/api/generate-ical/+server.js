059d7a39804a5ad2eeb32dd9e92a6470d9ccbcb1
Runkai Zhang <runkai@duck.com>
2022-10-06 00:04:24 -0400

    Add icalendar generator api

---
+++ src/routes/api/generate-ical/+server.js
@@ -0,0 +1,21 @@
+import ical from 'ical-generator';
+
+export async function GET() {
+    const calendar = ical({name: 'my first iCal'});
+    const startTime = new Date();
+    const endTime = new Date();
+    endTime.setHours(startTime.getHours()+1);
+    calendar.createEvent({
+        start: startTime,
+        end: endTime,
+        summary: 'Example Event',
+        description: 'It works ;)',
+        location: 'my room',
+        url: 'http://sebbo.net/'
+    });
+
+    const res = new Response(await calendar);
+    res.headers.set('Content-Type', 'text/calendar;charset=utf-8');
+
+    return res;
+}
