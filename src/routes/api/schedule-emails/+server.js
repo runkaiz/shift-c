16a25e154452fdb03465b37775637927b86ef969
Runkai Zhang <runkai@duck.com>
2022-11-02 17:29:34 -0400

    Setup basic email scheduling system for the entire intervention plan

--- src/routes/api/schedule-emails/+server.js
+++ src/routes/api/schedule-emails/+server.js
@@ -1,5 +1,6 @@
 // TODO: Complete the email scheduling system.
 
+import moment from 'moment/moment';
 import sgMail from '@sendgrid/mail';
 // sgMail.setApiKey(import.meta.env.VITE_SENDGRID);
 
@@ -7,16 +8,22 @@ import sgMail from '@sendgrid/mail';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
-	const msg = {
-		to: 'test@example.com',
-		from: 'test@example.com',
-		subject: 'Sending with SendGrid is Fun',
-		text: 'and easy to do anywhere, even with Node.js',
-		html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-        send_at: ''
-	};
-	console.log('Form submitted');
-	// const output = await sgMail.send(msg);
+    const interventionDays = 10; // Get this from an input
+    const preferredTime = moment(new Date().toISOString(), moment.ISO_8601); // Get a time of the day that the user wants emails on
+
+    for (let i = 0; i < interventionDays; i++) {
+        const msg = {
+            to: 'test@example.com',
+            from: 'test@example.com',
+            subject: 'Sending with SendGrid is Fun',
+            text: 'and easy to do anywhere, even with Node.js',
+            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+            send_at: preferredTime.clone().add(i, "days").valueOf()
+        };
+        console.log('Form submitted');
+        // const output = await sgMail.send(msg);
+    }
+
 	return {
 		body: output
 	};
