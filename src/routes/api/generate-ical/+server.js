d64db01be561e53d62c81b256efe028443065e65
Runkai Zhang <runkai@duck.com>
2022-11-30 21:09:03 -0500

    :skull:

--- src/routes/api/generate-ical/+server.js
+++ src/routes/api/generate-ical/+server.js
@@ -20,21 +20,13 @@ export async function GET({ url }) {
 
     const tz = moment().utcOffset() - Number(url.searchParams.get('tz'));
 
-	// const interventionStart = moment(url.searchParams.get('n'), moment.ISO_8601).add(1, 'days').subtract(tz, 'minutes');
+	const interventionStart = moment(url.searchParams.get('n'), moment.ISO_8601).add(1, 'days').add(tz, 'minutes');
 
-	// const currentWakeTime = moment(url.searchParams.get('cWake'), ['HH:mm']).subtract(tz, 'minutes');
-	// const currentSleepTime = moment(url.searchParams.get('cSleep'), ['HH:mm']).subtract(tz, 'minutes');
+	const currentWakeTime = moment(url.searchParams.get('cWake'), ['HH:mm']).add(tz, 'minutes');
+	const currentSleepTime = moment(url.searchParams.get('cSleep'), ['HH:mm']).add(tz, 'minutes');
 
-	// const targetWakeTime = moment(url.searchParams.get('gWake'), ['HH:mm']).subtract(tz, 'minutes');
-	// const targetSleepTime = moment(url.searchParams.get('gSleep'), ['HH:mm']).subtract(tz, 'minutes');
-
-    const interventionStart = moment(url.searchParams.get('n'), moment.ISO_8601).add(1, 'days');
-
-	const currentWakeTime = moment(url.searchParams.get('cWake'), ['HH:mm']);
-	const currentSleepTime = moment(url.searchParams.get('cSleep'), ['HH:mm']);
-
-	const targetWakeTime = moment(url.searchParams.get('gWake'), ['HH:mm']);
-	const targetSleepTime = moment(url.searchParams.get('gSleep'), ['HH:mm']);
+	const targetWakeTime = moment(url.searchParams.get('gWake'), ['HH:mm']).add(tz, 'minutes');
+	const targetSleepTime = moment(url.searchParams.get('gSleep'), ['HH:mm']).add(tz, 'minutes');
 
 	// Algorithm Time
 	if (
