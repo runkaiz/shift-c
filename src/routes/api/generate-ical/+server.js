adf15fa907c1ad6a5f6033940a3d298b21f911b4
Runkai Zhang <runkai@duck.com>
2022-11-30 21:50:42 -0500

    Please work

--- src/routes/api/generate-ical/+server.js
+++ src/routes/api/generate-ical/+server.js
@@ -79,7 +79,7 @@ export async function GET({ url }) {
 		}
 		sleepIntervention[0].year(interventionStart.year());
 		sleepIntervention[0].month(interventionStart.month());
-        if (sleepTimeModded) {
+        if (sleepTimeModded && (targetSleepTime.diff(currentSleepTime, "hours") < 12)) {
             sleepIntervention[0].date(interventionStart.date()).add(1, "day");
         } else {
             sleepIntervention[0].date(interventionStart.date());
