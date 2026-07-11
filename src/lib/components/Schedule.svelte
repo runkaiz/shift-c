b5b072ad5194c507da27ca9d2b62f6c5b22b4965
yych42 <43839115+yych42@users.noreply.github.com>
2026-07-11 18:49:07 +0800

    chore: remove dead code, fix duplicated day-3 entry in Schedule demo
    - Schedule.svelte: day9 through day14 were declared and had setDate()
      called on them but were never referenced in either the web or print
      template (only day1-day8 render). Also fix a duplicated
      `<WakeTime date={day3} .../>` line that was standing in for what
      every other day has: a WakeTime followed by a SleepTime entry.
    - survey/current-schedule/+page.svelte: delete a 19-line commented-out
      calcBedtime() function copy-pasted from the sibling ideal-schedule
      page, unused here since this page asks for both times directly.

--- src/lib/components/Schedule.svelte
+++ src/lib/components/Schedule.svelte
@@ -12,12 +12,6 @@
 	let day6 = new Date(day1);
 	let day7 = new Date(day1);
 	let day8 = new Date(day1);
-	let day9 = new Date(day1);
-	let day10 = new Date(day1);
-	let day11 = new Date(day1);
-	let day12 = new Date(day1);
-	let day13 = new Date(day1);
-	let day14 = new Date(day1);
 
 	day2.setDate(day1.getDate() + 1);
 	day3.setDate(day1.getDate() + 2);
@@ -26,12 +20,6 @@
 	day6.setDate(day1.getDate() + 5);
 	day7.setDate(day1.getDate() + 6);
 	day8.setDate(day1.getDate() + 7);
-	day9.setDate(day1.getDate() + 8);
-	day10.setDate(day1.getDate() + 9);
-	day11.setDate(day1.getDate() + 10);
-	day12.setDate(day1.getDate() + 11);
-	day13.setDate(day1.getDate() + 12);
-	day14.setDate(day1.getDate() + 13);
 </script>
 
 <!-- Web view -->
@@ -44,7 +32,7 @@
 			<WakeTime date={day2.toDateString()} time="10:00AM" />
 			<SleepTime date={day2.toDateString()} time="9:20PM" />
 			<WakeTime date={day3.toDateString()} time="10:00AM" />
-			<WakeTime date={day3.toDateString()} time="10:00AM" />
+			<SleepTime date={day3.toDateString()} time="9:20PM" />
 			<SleepTime date={day4.toDateString()} time="9:20PM" />
 			<WakeTime date={day4.toDateString()} time="10:00AM" />
 			<SleepTime date={day5.toDateString()} time="9:20PM" />
@@ -68,7 +56,7 @@
 			<WakeTime date={day2.toDateString()} time="10:00AM" />
 			<SleepTime date={day2.toDateString()} time="9:20PM" />
 			<WakeTime date={day3.toDateString()} time="10:00AM" />
-			<WakeTime date={day3.toDateString()} time="10:00AM" />
+			<SleepTime date={day3.toDateString()} time="9:20PM" />
 			<SleepTime date={day4.toDateString()} time="9:20PM" />
 			<WakeTime date={day4.toDateString()} time="10:00AM" />
 			<SleepTime date={day5.toDateString()} time="9:20PM" />
